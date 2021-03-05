e8db493292b24b7f5dde7c1bf9e8cd2071dbcf18
David Freitas <62804530+dvdfreitas@users.noreply.github.com>
2021-03-05 09:25:00 +0000

    first commit

---
+++ database/migrations/2021_03_05_091242_create_readings_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateReadingsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('readings', function (Blueprint $table) {
+            $table->id();
+            $table->bigInteger('user_id');
+            $table->integer('temperatura');
+            $table->integer('caudal_acumulado');
+            $table->integer('aparelho_id');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('readings');
+    }
+}
